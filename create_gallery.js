5e81117bc0042c1bcdd142321270011779ca682b
Simone <simone@getsensibill.com>
2018-04-20 10:23:24 -0400

    Improved gallery generation and straighten packages

--- create_gallery.js
+++ create_gallery.js
@@ -7,8 +7,8 @@ const cloudinary_config = require( "../cloudinary_config.json" );
 cloudinary.config(cloudinary_config);
 const folder = "Darwin";
 
-const sizes = ["fig-75", "fig-25", "fig-25", "fig-25", "fig-50", "fig-25", "fig-25", "fig-25", "fig-25"];
-const extra_classes = ["right", "", "", "clear", "", "right", "right", "right", "right"];
+const sizes = ["fig-100", "fig-50", "fig-50", "fig-33", "fig-33", "fig-33", "fig-75", "fig-25", "fig-25", "fig-25", "fig-50", "fig-25", "fig-25", "fig-25", "fig-25"];
+const extra_classes = ["", "left", "right", "", "", "", "right", "", "", "clear", "", "right", "right", "right", "right"];
 const images = [];
 
 cloudinary.v2.api.resources({folder: folder, max_results: 500, angle: "exif"}, function(error, result) {
@@ -16,6 +16,9 @@ cloudinary.v2.api.resources({folder: folder, max_results: 500, angle: "exif"}, f
   	result.resources.forEach(function(resource) {
   		images.push("{% image fancybox " + sizes[index] + " " + extra_classes[index] + " nocaption group:" + folder.toLowerCase() + " " + resource.url.replace("/upload/", "/upload/a_exif/") + " %}");
   		index++;
+  		if (index >= sizes.length) {
+  			index = 0;
+  		}
   	});
 
   	console.log("\n\
