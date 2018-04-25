fc9ce7f95fdb83611a914d720ff24b86f01ea650
Simone Giacomel <sgiacomel@gmail.com>
2018-04-24 22:48:51 -0400

    Fixed style

--- create_map.js
+++ create_map.js
@@ -1,13 +1,20 @@
 const puppeteer = require('puppeteer');
 const date = process.argv[2];
+let params = '';
+if(date) {
+	params = '&dates=' + date;
+	name = date.split('/').join('-');
+}
+else {
+	name = "map-all";
+}
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   page.setDefaultNavigationTimeout(60000);
-  await page.goto('http://localhost:4000/map/?embed=true&dates=' + date);
+  await page.goto('http://localhost:4000/map/?embed=true)' + params);
   await page.waitForSelector('#map-loaded');
   const text = await page.evaluate(el => el.innerHTML, await page.$('#map-loaded'));
-  console.log(text);
-  await page.screenshot({path: date.split('/').join('-') + '.jpg'});
+  await page.screenshot({path: name + '.jpg'});
   await browser.close();
 })();
